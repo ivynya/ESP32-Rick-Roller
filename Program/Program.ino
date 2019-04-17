aeb184fca6229a40b5142320126e278b65d9a37c
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-04-17 00:25:12 -0700

    SPIFFS file loading
    video/gif loadable via SPIFFS

--- Program/Program.ino
+++ Program/Program.ino
@@ -20,7 +20,7 @@ DNSServer dnsServer;
 AsyncWebServer server(80);
 
 void onRequest(AsyncWebServerRequest *request){
-  request->send(200, "text/html", "<!DOCTYPE html><html><head><title>ESP32</title></head><body><h1>Big</h1></body></html>");
+  request->send(200, "text/html", "<!DOCTYPE html><html><head><title>ESP32</title></head><body><h1>Big</h1><img src=\"rick.gif\"><video controls src=\"astley.mp4\" /></body></html>");
 }
 
 void setup() {
@@ -29,12 +29,26 @@ void setup() {
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP(ssid);
 
+  // Initialize SPIFFS
+  if(!SPIFFS.begin(true)){
+    Serial.println("An error has occurred while mounting SPIFFS");
+    return;
+  }
+
   // Start DNS server with * (will redirect all)
   dnsServer.start(DNS_PORT, "*", apIP);
 
   // Redirected DNS queries will get sent here
   server.onNotFound(onRequest);
-
+  
+  // Route to load G O D M O D E
+  server.on("/rick.gif", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/rick.gif");
+  });
+  server.on("/astley.mp4", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/astley.mp4");
+  });
+  
   server.begin();
 }
 
